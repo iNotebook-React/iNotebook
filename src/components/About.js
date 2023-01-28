ca4ec66accf055e5853a6f5406412eaef5c0211a
prathambhasin <57487767+prathambhasin@users.noreply.github.com>
2023-01-28 20:02:16 +0530

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => { 
+    return (
+        <div>
+            <h2>Welcome to iNotebook.</h2>
+            <p>iNotebook is cloud based notebook which can be used to keep your notes online and we can access it from anywhere in the world.</p>        
+        </div>
+    )
+}
+
+export default About
